b878e9e951f67775520c014911e8f121a39c18a5
Mohamed Omar <mo455790@gmail.com>
2026-08-09 20:43:16 +0300

    add more features

--- main.c
+++ main.c
@@ -1,11 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stack.h>
+#include "stack.h"
+
+void printElement(StackEntry e)
+{
+    printf("%d\n", e);
+}
 
 
 int main()
 {
     stack s;
-    createStack(&s);
+    createStack(&s); 
+
+    printf("--- Pushing Elements ---\n");
+    push(10, &s);
+    push(20, &s);
+    push(30, &s);
+    push(40, &s);
+    push(50, &s);
+    printf("Elements (10, 20, 30, 40, 50) have been pushed.\n");
+    printf("Current Size: %d\n", stackSize(&s));
+
+    
+    printf("\n--- Traversing Stack ---\n");
+    traverseStack(&s, printElement);
+
+    printf("\n--- Checking Top ---\n");
+    StackEntry topVal;
+    stackTop(&topVal, &s);
+    printf("Top Element is: %d\n", topVal);
+
+    printf("\n--- Popping Element ---\n");
+    StackEntry poppedVal;
+    pop(&poppedVal, &s);
+    printf("Popped Element: %d\n", poppedVal);
+    printf("Size after Pop: %d\n", stackSize(&s));
+
+    printf("\n--- Clearing Stack ---\n");
+    clearStack(&s);
+    
+    if (stackEmpty(&s))
+    {
+        printf("Stack is now completely empty. Size: %d\n", stackSize(&s));
+    }
+
     return 0;
 }
